772409fe51e63229496d74dec2924757494a244e
[OhMyjitui] <[christmasgbc@qq.com]>
2016-07-11 21:57:15 +0800

    C6

---
+++ C5/i/kernel/i8259.c
@@ -0,0 +1,34 @@
+#include "type.h" 
+#include "const.h" 
+#include "proto.h" 
+#include "protect.h" 
+
+
+PUBLIC void init_8259A()
+{ 
+    out_byte(INT_M_CTL, 0x11);
+    out_byte(INT_S_CTL, 0x11);
+
+    out_byte(INT_M_CTLMASK, INT_VECTOR_IRQ0);
+    out_byte(INT_S_CTLMASK, INT_VECTOR_IRQ8); 
+
+    out_byte(INT_M_CTLMASK, 0x4);
+    out_byte(INT_S_CTLMASK, 0x2); 
+
+    out_byte(INT_M_CTLMASK, 0x1);
+    out_byte(INT_S_CTLMASK, 0x1);
+
+    out_byte(INT_M_CTLMASK, 0xFD);
+    out_byte(INT_S_CTLMASK, 0xFF); 
+
+}
+
+/*======================================================================*
+                           spurious_irq
+ *======================================================================*/
+PUBLIC void spurious_irq(int irq)
+{
+        disp_str("spurious_irq: ");
+        disp_int(irq);
+        disp_str("\n");
+}
